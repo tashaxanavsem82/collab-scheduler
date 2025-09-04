97838848f0ef632694dd5ead95f8ccb78c16781d
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-04 12:44:53 -0500

    Use modern MongoDB connection options with updated mongoose method

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 }).then(() => {
   console.log('Connected to MongoDB');
 }).catch(err => console.error(err));
