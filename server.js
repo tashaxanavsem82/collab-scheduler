4bd596a0c1190a8efe79872dd183ae29c2717fc0
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-04 12:40:27 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+  res.send('Collab Scheduler API');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
